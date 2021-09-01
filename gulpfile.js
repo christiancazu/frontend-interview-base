b7db126384114d1c0a5ac3046b891c62bb2685d4
Alejandro <alejandrolopez.gd@gmail.com>
2021-09-01 09:12:36 -0400

    fix: remove unnecessary features

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-var gulp          = require('gulp');
-var notify        = require('gulp-notify');
-var source        = require('vinyl-source-stream');
-var browserify    = require('browserify');
-var babelify      = require('babelify');
-var ngAnnotate    = require('browserify-ngannotate');
-var browserSync   = require('browser-sync').create();
-var rename        = require('gulp-rename');
+var gulp = require('gulp');
+var notify = require('gulp-notify');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var ngAnnotate = require('browserify-ngannotate');
+var browserSync = require('browser-sync').create();
+var rename = require('gulp-rename');
 var templateCache = require('gulp-angular-templatecache');
-var uglify        = require('gulp-uglify');
-var merge         = require('merge-stream');
+var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
 
 // Where our files are located
-var jsFiles   = "src/js/**/*.js";
+var jsFiles = "src/js/**/*.js";
 var viewFiles = "src/js/**/*.html";
 
-var interceptErrors = function(error) {
+var interceptErrors = function (error) {
   var args = Array.prototype.slice.call(arguments);
 
   // Send error to notification center with gulp-notify
@@ -28,48 +28,48 @@ var interceptErrors = function(error) {
 };
 
 
-gulp.task('browserify', ['views'], function() {
+gulp.task('browserify', ['views'], function () {
   return browserify('./src/js/app.js')
-      .transform(babelify, {presets: ["es2015"]})
-      .transform(ngAnnotate)
-      .bundle()
-      .on('error', interceptErrors)
-      //Pass desired output filename to vinyl-source-stream
-      .pipe(source('main.js'))
-      // Start piping stream to tasks!
-      .pipe(gulp.dest('./build/'));
+    .transform(babelify, { presets: ["es2015"] })
+    .transform(ngAnnotate)
+    .bundle()
+    .on('error', interceptErrors)
+    //Pass desired output filename to vinyl-source-stream
+    .pipe(source('main.js'))
+    // Start piping stream to tasks!
+    .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
   return gulp.src("src/index.html")
-      .on('error', interceptErrors)
-      .pipe(gulp.dest('./build/'));
+    .on('error', interceptErrors)
+    .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('views', function() {
+gulp.task('views', function () {
   return gulp.src(viewFiles)
-      .pipe(templateCache({
-        standalone: true
-      }))
-      .on('error', interceptErrors)
-      .pipe(rename("app.templates.js"))
-      .pipe(gulp.dest('./src/js/config/'));
+    .pipe(templateCache({
+      standalone: true
+    }))
+    .on('error', interceptErrors)
+    .pipe(rename("app.templates.js"))
+    .pipe(gulp.dest('./src/js/config/'));
 });
 
 // This task is used for building production ready
 // minified JS/CSS files into the dist/ folder
-gulp.task('build', ['html', 'browserify'], function() {
+gulp.task('build', ['html', 'browserify'], function () {
   var html = gulp.src("build/index.html")
-                 .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'));
 
   var js = gulp.src("build/main.js")
-               .pipe(uglify())
-               .pipe(gulp.dest('./dist/'));
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
 
-  return merge(html,js);
+  return merge(html, js);
 });
 
-gulp.task('default', ['html', 'browserify'], function() {
+gulp.task('default', ['html', 'browserify'], function () {
 
   browserSync.init(['./build/**/**.**'], {
     server: "./build",
